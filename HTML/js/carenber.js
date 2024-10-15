2bd86e580084b0342c228e7a2d560f8022520809
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-10-15 15:51:08 +0900

    カレンダー作成

---
+++ HTML/js/carenber.js
@@ -0,0 +1,41 @@
+'usr strict'
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const firstDate = new Date(year, month -1, 1);
+const firstDay = firstDate.getDay(); // 曜日のデータ
+const lastDate = new Date(year, month, 0);
+const lastDayCount = lastDate.getDate();
+
+let dayCount = 1;
+let createHtml = '';
+
+createHtml = '<h1>' + year + '/' + month + '</h1>'
+createHtml += '<table>' + '<tr>';
+
+const weeks = ['日','月','火','水','木','金','土'];
+for (let i = 0; i < weeks.length; i++) {
+  createHtml += '<td>' + weeks[i] + '</td>';
+}
+createHtml += '</tr>';
+
+for (let n = 0; n < 6; n++) {
+  createHtml += '<tr>'
+    for (let d = 0; d < 7; d++) {
+      if (n == 0 && d < firstDay) {
+        createHtml += '<td></td>';
+      } else if (dayCount > lastDayCount) {
+        createHtml += '<td></td>';
+      } else {
+        createHtml += '<td>' + dayCount + '</td>';
+        dayCount++;
+      }
+    }
+  createHtml += '</tr>';
+}
+createHtml += '</table>';
+
+document.querySelector('#calenber').innerHTML = createHtml;
+
+console.log(createHtml);
